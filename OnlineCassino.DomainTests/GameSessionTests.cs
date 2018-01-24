e7104f47a780e67c5834084eb5faa8bdabdc2d2b
Leonardo Bruno Lima <leonardo.bruno@outlook.com>
2018-01-24 19:47:32 -0300

    added new tests on domain objects

---
+++ OnlineCassino.DomainTests/GameSessionTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace OnlineCassino.Domain.Tests
+{
+    [TestClass()]
+    public class GameSessionTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ShouldFailCreateGameSessionWithWrongParams()
+        {
+            var gameSession = new GameSession(null, null);
+        }
+
+        [TestMethod()]
+        public void ShouldCreateGameSessionWithCorrectParams()
+        {
+            var gameSession = new GameSession(new Game("test game", 1, DateTime.Now.AddDays(15), GameCategory.ClassicSlots), new User("Leo", "xxxxxxxxxxxxxx"));
+
+            Assert.IsNotNull(gameSession);
+        }
+    }
+}
---
+++ OnlineCassino.DomainTests/UserTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace OnlineCassino.Domain.Tests
+{
+    [TestClass()]
+    public class UserTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ShouldFailCreateUserWithWrongParams()
+        {
+            var user = new User(null, null);
+        }
+
+        [TestMethod()]
+        public void ShouldCreateUserWithCorrectParams()
+        {
+            var userName = "Leonardo";
+            var user = new User(userName, "xxxxxxxxxxxxxx");
+
+            Assert.IsNotNull(user);
+            Assert.AreEqual(user.Name, userName);
+        }
+    }
+}
